dc0993a8b573a5cc054f4df95fe3d98c4e3a86b7
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-10 22:19:50 +0900

    shell script for DHT11

---
+++ DHT11.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
+sleep 1
+cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
