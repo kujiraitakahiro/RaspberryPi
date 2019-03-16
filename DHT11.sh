43af4ccc329029dd3588341e41e0fd079e3d303e
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-16 14:52:24 +0900

    fix for getting humidityrelative_input
    When humidityrelative_input was *0000(for example, 500000/400000),
    scripts return 5%0/4%0.
    Solved:Adding sed’s regular expressions.

--- DHT11.sh
+++ DHT11.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed -r s/000$/'%'/g
 sleep 1
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed -r s/000$/'%'/g
 sleep 1
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed -r s/000$/'%'/g
 sleep 1
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed -r s/000$/'%'/g
 sleep 1
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
-sleep 1
-cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed s/000/'%'/g
+cat /sys/bus/iio/devices/iio\:device0/in_humidityrelative_input | sed -r s/000$/'%'/g
 sleep 1
 cat /sys/bus/iio/devices/iio\:device0/in_temp_input | sed s/000/'℃'/g
 sleep 1
