9b2766aa507cf2bd6c37e7963f8c81ee3c6d5481
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-17 21:26:44 +0900

    Getting Temp by LM61BIZ and MPC3002

---
+++ temp_analog.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+#Import wiringgpi for Using GPIO, Import time for Using Sleep function.
+import wiringpi as pi, time
+
+#Specify SPI channel on MPC3002(ADconverter)
+SPI_CH = 0
+READ_CH = 0
+
+pi.wiringPiSPISetup(SPI_CH, 1000000 )
+
+#for LM61BIZ
+while True:
+	Buffer = 0x6800 | (0x1800 * READ_CH )
+	Buffer = Buffer.to_bytes( 2, byteorder='big' )
+	pi.wiringPiSPIDataRW( SPI_CH, Buffer )
+	value = ( Buffer[0] * 256 + Buffer[1] ) & 0x3ff
+	volt = value * 3.3 / 1023
+	Temp = volt * 100
+	print ("Temp:", Temp , "℃")
+	time.sleep(1)
