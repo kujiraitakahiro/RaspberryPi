9f22fdab6cb42bbc8d95325cfcc20e67561263a8
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-18 16:52:47 +0900

    change channel ,etc

--- data_analog.py
+++ data_analog.py
@@ -5,7 +5,7 @@ import spidev
 import time
 
 spi = spidev.SpiDev()
-spi.open(0, 0)
+spi.open(0, 1)
 spi.max_speed_hz = 1000000 #must spidev3.3
 
 while True:
@@ -13,7 +13,7 @@ while True:
     resp = spi.xfer2([0x78, 0x00])
     value = (resp[0] * 256 + resp[1]) & 0x3ff
     volt = value * 3.3 / 1023
-    print(value,volt)
+    print("Value: ",value," Volt: ",volt,"V")
     time.sleep(0.5)
   except KeyboardInterrupt:
     break
