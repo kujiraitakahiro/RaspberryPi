22eaba8e215cd7ad14e8e320a355ff915cd89ea3
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-18 15:59:03 +0900

    add data_analog.py ,etc

---
+++ data_analog.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+spi.max_speed_hz = 1000000 #must spidev3.3
+
+while True:
+  try:
+    resp = spi.xfer2([0x78, 0x00])
+    value = (resp[0] * 256 + resp[1]) & 0x3ff
+    volt = value * 3.3 / 1023
+    print(value,volt)
+    time.sleep(0.5)
+  except KeyboardInterrupt:
+    break
+
+spi.close()
--- temp_analog.py
+++ temp_analog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-#Import wiringgpi for Using GPIO, Import time for Using Sleep function.
+#Import wiringgpi for Control GPIOs, Import time for Using Sleep function.
 import wiringpi as pi
 import time
 
@@ -9,15 +9,15 @@ import time
 SPI_CH = 0
 READ_CH = 0
 
-pi.wiringPiSPISetup(SPI_CH, 1000000 )
+pi.wiringPiSPISetup( SPI_CH, 1000000 )
 
 #for LM61BIZ
 while True:
-	Buffer = 0x6800 | (0x1800 * READ_CH )
+	Buffer = 0x6800 | ( 0x1800 * READ_CH )
 	Buffer = Buffer.to_bytes( 2, byteorder='big' )
 	pi.wiringPiSPIDataRW( SPI_CH, Buffer )
 	Value = ( Buffer[0] * 256 + Buffer[1] ) & 0x3ff
 	Volt = Value * 3.3 / 1023
-	Temp = (Volt-0.6) * 100
-	print ("Temperature:", Temp , "℃")
+	Temp = Volt * 100
+	print ("Temperature: ", Temp , "℃")
 	time.sleep(1)
