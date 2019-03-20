1b1b7546e547f8dc95bb32c7b185128f8e6c9a6a
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2019-03-20 18:36:16 +0900

    change Offset position

--- temp_analog.py
+++ temp_analog.py
@@ -12,12 +12,13 @@ READ_CH = 0
 pi.wiringPiSPISetup( SPI_CH, 1000000 )
 
 #for LM61BIZ
+Offset = ( 0.6 / 3.3 ) * 1023 #LM61BIZ offset is 0℃ = 600mV(600mV)
+
 while True:
 	Buffer = 0x6800 | ( 0x1800 * READ_CH )
 	Buffer = Buffer.to_bytes( 2, byteorder='big' )
 	pi.wiringPiSPIDataRW( SPI_CH, Buffer )
 	Value = ( Buffer[0] * 256 + Buffer[1] ) & 0x3ff
-	Offset = ( 0.6 / 3.3 ) * 1023 #LM61BIZ offset is 0℃ = 600mV(600mV)
 	Volt = ( Value - Offset ) * 3.3 / 1023
 	Temp = round(Volt * 100,3)
 	print ("Value: ",Value," Volt: ",Volt," Temperature: ", Temp , "℃")
